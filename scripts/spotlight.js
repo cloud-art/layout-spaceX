af30929810d8bb6da8cada7439b45142942ad5f4
cloud-art <artyushenko.k.r1@gmail.com>
2022-07-13 17:29:18 +0300

    removed useless file

--- scripts/spotlight.js
+++
@@ -1,7 +0,0 @@
-$(window).mousemove(function(e){
-    let w = $('.spotlight').innerWidth(),
-    h = $('.spotlight').innerHeight(),
-    t = e.pageY - $('.spotlight').offset().top,
-    l = e.pageX - $('.spotlight').offset().left;
-$('.spotlight').css('background-image', 'radial-gradient(circle at '+ (l / w * 100) +'% '+ (t / h * 100) +'%, transparent 80px, rgba(0, 0, 0, 0.8) 120px)');
-});  
